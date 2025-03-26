8ff5b53f8dd00d17ee55650146a6c6cd115a205f
Filip Dudziński <filip.dudzinski.90@gmail.com>
2025-03-26 21:15:36 +0100

    Add DI library

---
+++ static_di/__init__.py
@@ -0,0 +1,7 @@
+from .class_dependency import *
+from .value_dependency import *
+from .scope import *
+from .dependency_injector import DependencyInjectorFactory as DependencyInjector
+from .default_config import *
+
+__all__ = ["DependencyInjector"]
